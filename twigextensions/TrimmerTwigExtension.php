9a821825922186b4004244e50f34f75150c6e9d4
Roi Kingon <rkingon@gmail.com>
2014-01-05 15:42:40 -0800

    new strip tags function to add spaces between block elements and remove any non-content tags completely

--- twigextensions/TrimmerTwigExtension.php
+++ twigextensions/TrimmerTwigExtension.php
@@ -33,7 +33,7 @@ class TrimmerTwigExtension extends \Twig_Extension
 		$word = (!in_array(strtolower($word), array("no","false")) || $word !== false);
 		
 		// Remove HTML tags.
-		$str = strip_tags($str);
+		$str = $this->_strip_html_tags($str);
 		
 		// Only try and truncate / add ellipsis if our string length is longer than our length limit
 		if(strlen($str) > $length)
@@ -57,4 +57,36 @@ class TrimmerTwigExtension extends \Twig_Extension
 		return preg_replace("/\.\W*$/", "", $str);
 	}
 	
+	private function _strip_html_tags($text)
+	{
+		$text = preg_replace(
+			array(
+				// Remove invisible content
+				'@<head[^>]*?>.*?</head>@siu',
+				'@<style[^>]*?>.*?</style>@siu',
+				'@<script[^>]*?.*?</script>@siu',
+				'@<object[^>]*?.*?</object>@siu',
+				'@<embed[^>]*?.*?</embed>@siu',
+				'@<applet[^>]*?.*?</applet>@siu',
+				'@<noframes[^>]*?.*?</noframes>@siu',
+				'@<noscript[^>]*?.*?</noscript>@siu',
+				'@<noembed[^>]*?.*?</noembed>@siu',
+				// Add line breaks before and after blocks
+				'@</?((address)|(blockquote)|(center)|(del))@iu',
+				'@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+				'@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+				'@</?((table)|(th)|(td)|(caption))@iu',
+				'@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+				'@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+				'@</?((frameset)|(frame)|(iframe))@iu',
+				'@</?((article)|(section)|(address)|(header)|(footer)|(figure)|(nav)|(aside))@iu',
+			),
+			array(
+				' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+				"\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+				"\n\$0", "\n\$0", "\n\$0",
+			),
+			$text );
+		return strip_tags($text);
+	}
 }
